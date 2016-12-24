9a616cf1a534d0d978d36a08e02329aa95654bba
Steve Hopkinson <steve@stevehopkinson.co.uk>
2016-12-24 14:21:50 +0000

    Add route and tests for home route

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).send('ok');
+});
+
 module.exports = app;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,5 @@
 const test = require('tape-catch');
+const request = require('supertest');
 const app = require('../src/app');
 
 test('Server should start', t => {
@@ -6,4 +7,18 @@ test('Server should start', t => {
     t.pass('Server starts without error');
     server.close(t.end);
   });
+});
+
+test('App should respond to / route with a 200 status code', t => {
+  request(app)
+    .get('/')
+    .expect(200)
+    .end(err => {
+      if (err) {
+        t.error(err);
+      } else {
+        t.pass('App responded to request for / with status code 200');
+      }
+      t.end();
+    });
 });
