176e5ddc4d133ffc9dac4e6e8aa53cc42464f458
Steve Hopkinson <steve@stevehopkinson.co.uk>
2017-01-04 00:03:20 +0000

    Install serve-static and set up caching on server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const serveStatic = require('serve-static');
 const app = express();
 
-app.use(express.static('public'));
+app.use(serveStatic('public', { maxAge: '1d' }));
 
 module.exports = app;
