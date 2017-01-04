f87a44a8978e6c508e522cfa75589e9891165a24
Steve Hopkinson <steve@stevehopkinson.co.uk>
2017-01-04 00:09:23 +0000

    Install compression middleware and add to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 const serveStatic = require('serve-static');
 const app = express();
+
+app.use(compression());
 
 app.use(serveStatic('public', { maxAge: '1d' }));
 
